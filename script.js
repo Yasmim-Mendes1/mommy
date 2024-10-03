37a9dd8cc0d1b9389c484371985f718215cb53c4
Yasmim Mendes <157243378+Yasmim-Mendes1@users.noreply.github.com>
2024-10-03 06:23:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 2,
+  spaceBetween: 10 ,
+  centeredSlides: true, fade: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
